c970f752a20577dbb719c780eea0f9516ce6815e
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-06-17 13:59:48 +0200

    Update soundplayer.py

--- Repl/soundplayer.py
+++ Repl/soundplayer.py
@@ -1,12 +1,9 @@
-import os
+import time#, playsound
 
-os.system("pip3 install playsound")
-
-import time, playsound
 class SoundPlayer:
 	def __init__(self, path, game):
 		self.path = path
 		self.game = game
 	def play(self):
 		if self.game.settings_button.dict["audio"]:
-			playsound.playsound(self.path)
+			pass#playsound.playsound(self.path)
