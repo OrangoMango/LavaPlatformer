77b3200318abda92d788133de2b43d24e66e990c
Orango Mango <61402409+OrangoMango@users.noreply.github.com>
2020-06-17 14:08:21 +0200

    Delete soundplayer.py

--- Repl/soundplayer.py
+++
@@ -1,9 +0,0 @@
-import time#, playsound
-
-class SoundPlayer:
-	def __init__(self, path, game):
-		self.path = path
-		self.game = game
-	def play(self):
-		if self.game.settings_button.dict["audio"]:
-			pass#playsound.playsound(self.path)
